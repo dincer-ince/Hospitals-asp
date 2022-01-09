ce913a4f8f5f18661f9f5b720d3f6f300ad9b113
Dincer İnce <dincer.ince00@gmail.com>
2022-01-09 08:40:36 +0300

    asd

---
+++ Models/Hospital.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace covid_backend.Models
+{
+    public partial class Hospital
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public int Fid { get; set; }
+        public long? Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int? Zip { get; set; }
+        public int? Zip4 { get; set; }
+        public string Telephone { get; set; }
+        public string Type { get; set; }
+        public string Status { get; set; }
+        public int? Population { get; set; }
+        public string County { get; set; }
+        public int? Countyfips { get; set; }
+        public string Country { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public int? NaicsCode { get; set; }
+        public string NaicsDesc { get; set; }
+        public string Source { get; set; }
+        public string Sourcedate { get; set; }
+        public string ValMethod { get; set; }
+        public string ValDate { get; set; }
+        public string Website { get; set; }
+        public string StateId { get; set; }
+        public string AltName { get; set; }
+        public int? StFips { get; set; }
+        public string Owner { get; set; }
+        public int? TtlStaff { get; set; }
+        public int? Beds { get; set; }
+        public string Trauma { get; set; }
+        public char Helipad { get; set; }
+    }
+}
---
+++ Models/hospitalContext.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace covid_backend.Models
+{
+    public partial class hospitalContext : DbContext
+    {
+        public hospitalContext()
+        {
+        }
+
+        public hospitalContext(DbContextOptions<hospitalContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Hospital> Hospitals { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=2801;Database=hospital;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "English_Europe.1252");
+
+            modelBuilder.Entity<Hospital>(entity =>
+            {
+                entity.HasKey(e => e.Fid)
+                    .HasName("hospitals_pkey");
+
+                entity.ToTable("hospitals");
+
+                entity.Property(e => e.Fid)
+                    .ValueGeneratedNever()
+                    .HasColumnName("FID");
+
+                entity.Property(e => e.Address).HasColumnName("ADDRESS");
+
+                entity.Property(e => e.AltName).HasColumnName("ALT_NAME");
+
+                entity.Property(e => e.Beds).HasColumnName("BEDS");
+
+                entity.Property(e => e.City).HasColumnName("CITY");
+
+                entity.Property(e => e.Country).HasColumnName("COUNTRY");
+
+                entity.Property(e => e.County).HasColumnName("COUNTY");
+
+                entity.Property(e => e.Countyfips).HasColumnName("COUNTYFIPS");
+
+                entity.Property(e => e.Helipad)
+                    .HasColumnType("char(1)")
+                    .HasColumnName("HELIPAD");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Latitude).HasColumnName("LATITUDE");
+
+                entity.Property(e => e.Longitude).HasColumnName("LONGITUDE");
+
+                entity.Property(e => e.NaicsCode).HasColumnName("NAICS_CODE");
+
+                entity.Property(e => e.NaicsDesc).HasColumnName("NAICS_DESC");
+
+                entity.Property(e => e.Name).HasColumnName("NAME");
+
+                entity.Property(e => e.Owner).HasColumnName("OWNER");
+
+                entity.Property(e => e.Population).HasColumnName("POPULATION");
+
+                entity.Property(e => e.Source).HasColumnName("SOURCE");
+
+                entity.Property(e => e.Sourcedate).HasColumnName("SOURCEDATE");
+
+                entity.Property(e => e.StFips).HasColumnName("ST_FIPS");
+
+                entity.Property(e => e.State).HasColumnName("STATE");
+
+                entity.Property(e => e.StateId).HasColumnName("STATE_ID");
+
+                entity.Property(e => e.Status).HasColumnName("STATUS");
+
+                entity.Property(e => e.Telephone).HasColumnName("TELEPHONE");
+
+                entity.Property(e => e.Trauma).HasColumnName("TRAUMA");
+
+                entity.Property(e => e.TtlStaff).HasColumnName("TTL_STAFF");
+
+                entity.Property(e => e.Type).HasColumnName("TYPE");
+
+                entity.Property(e => e.ValDate).HasColumnName("VAL_DATE");
+
+                entity.Property(e => e.ValMethod).HasColumnName("VAL_METHOD");
+
+                entity.Property(e => e.Website).HasColumnName("WEBSITE");
+
+                entity.Property(e => e.Zip).HasColumnName("ZIP");
+
+                entity.Property(e => e.Zip4).HasColumnName("ZIP4");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
